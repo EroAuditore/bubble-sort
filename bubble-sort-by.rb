da663f4c93e569f7e28a6efa54c638d17447c95f
EroAuditore <alfa.fenrir@gmail.com>
2021-02-10 11:24:42 -0600

    Bubble sort by added to proyect and fixed roubocop errors

--- bubble-sort-by.rb
+++ bubble-sort-by.rb
@@ -1,20 +1,18 @@
-
-def bubble_sort_by
-    arr = ["hi", "hello", "hey"]
-    i=0
-    while i < arr.length - 1 
-        left = arr[i]
-        right = arr[i+1]
-        value = yield(left, right)
-        if  value > 0
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-            i=0
-        else
-         i += 1
-        end
+def bubble_sort_by(arr)
+  i = 0
+  while i < arr.length - 1
+    value = yield(arr[i], arr[i + 1])
+    if value.positive?
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      i = 0
+    else
+      i += 1
     end
-    puts "sorted_arr"
-    puts arr
+  end
+  arr
 end
-  
-bubble_sort_by { |left, right|  left.length - right.length}
+
+arr_sorted = bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
+
+puts 'sorted_arr'
+puts arr_sorted
--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,22 +1,18 @@
-##define method
-
-def bubble_sort(arr) 
-    i=0
-    while i < arr.length - 1
-        #compare left-rigth side
-        if arr[i] > arr[i+1]
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-            i=0
-        else
-            i += 1
-        end
+def bubble_sort(arr)
+  i = 0
+  while i < arr.length - 1
+    # compare left-rigth side
+    if arr[i] > arr[i + 1]
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      i = 0
+    else
+      i += 1
     end
-    return arr
+  end
+  arr
 end
 
+sorted_arr = bubble_sort([10, 2, 4, 1])
 
-
-sorted_arr = bubble_sort([10,2,4,1])
-
-puts("array sorted: ")
+puts('array sorted:')
 puts sorted_arr
